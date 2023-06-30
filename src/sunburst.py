9ac5b3fc109b6e3583e0929dd41f3e4c0217ac10
Mohammad Reza Gerami <mrgdatastu@gmail.com>
2023-06-29 23:57:08 -0400

    add sunburst and some new changes

--- src/sunburst.py
+++ src/sunburst.py
@@ -1,44 +1,98 @@
 import pandas as pd
-import numpy as np
 import plotly.express as px
+import webcolors
+import numpy as np
 
-def generate_sunburst(df):
+def generate_sunburst(data):
 
-    # Create an empty dictionary to store the users for each action
-    action_users = {}
-    # Select the columns of interest
-    dataset = df[['identifiant_visite', 'simulated_detailed_event', 'visit_page_num']]
+    # Fill NaN values with empty strings
+    data['simulated_detailed_event'].fillna('', inplace=True)
 
-    # Iterate over the dataset
-    for index, row in dataset.iterrows():
-        user = row['identifiant_visite']
-        action = row['simulated_detailed_event']
-        level = row['visit_page_num']
-        # Skip empty actions
-        if pd.isnull(action):
-            continue
-        
-        # Check if the action is already in the dictionary
-        if action in action_users:
-            action_users[action].append((user, level))
+    # Add level columns based on visit_page_num
+    levels = ['level1', 'level2', 'level3', 'level4', 'level5', 'level6', 'level7']
+    for i, level in enumerate(levels):
+        data[level] = np.where(data['visit_page_num'] >= i + 1, data['simulated_detailed_event'], None)
+
+    # Replace None values with "null"
+    data = data.fillna("null")
+
+    # Convert 'Account_Created_journey' column to numeric
+    data['Account_Created_journey'] = pd.to_numeric(data['Account_Created_journey'], errors='coerce')
+
+    # Filter out rows with missing or zero values in 'Account_Created_journey'
+    data = data[data['Account_Created_journey'].notna() & (data['Account_Created_journey'] != 0)]
+
+    # Calculate the total Account_Created_journey per level
+    level_counts = data.groupby(levels)['Account_Created_journey'].sum().reset_index()
+
+    # Calculate the percentage of Account_Created_journey per level
+    total_journey = level_counts['Account_Created_journey'].sum()
+    level_counts['Account_Created_percentage'] = (level_counts['Account_Created_journey'] / total_journey) * 100
+
+    # Generate the sunburst chart
+    fig = px.sunburst(
+        level_counts,
+        path=levels,
+        values='Account_Created_journey',
+        color='Account_Created_percentage',
+        color_continuous_scale='blues',
+        labels={'Account_Created_journey': 'Account Created Journey', 'Account_Created_percentage': 'Account Creation Percentage'},
+        hover_data={'Account_Created_percentage': ':.2f%<extra></extra>'},  # Display percentage in tooltip with 2 decimal places
+    )
+
+    # Remove null values from figure data
+    figure_data = fig['data'][0]
+    mask = np.char.find(figure_data.ids.astype(str), "null") == -1
+    figure_data.ids = figure_data.ids[mask]
+    figure_data.values = figure_data.values[mask]
+    figure_data.labels = figure_data.labels[mask]
+    figure_data.parents = figure_data.parents[mask]
+
+    # Update color based on path length
+    max_level = level_counts[levels].apply(lambda x: pd.to_numeric(x, errors='coerce').last_valid_index(), axis=1)
+    min_level = pd.to_numeric(max_level.min(), errors='coerce')
+    max_level_diff = pd.to_numeric(max_level.max(), errors='coerce') - min_level
+    normalized_max_level = (max_level - min_level) / max_level_diff if max_level_diff != 0 else 0
+
+    light_colors = []
+    for i, color in enumerate(px.colors.sequential.Blues):
+        color_str = str(color)
+        if color_str.startswith('#'):
+            light_rgb = tuple(int(min(255, c + 0.6 * (255 - c))) for c in px.colors.hex_to_rgb(color_str))
         else:
-            action_users[action] = [(user, level)]
-    # Find the maximum length of the user lists
-    max_length = 7 #max(len(users) for users in action_users.values()) #= 334
-    # Pad shorter user lists with NaN values
-    for users in action_users.values():
-        users.extend([(np.nan, np.nan)] * (max_length - len(users)))
-    action_users_sorted = sorted(action_users)
-    # Create a pivot table
-    pivot_table = df.pivot_table(index='identifiant_visite', columns='simulated_detailed_event', values='visit_page_num', aggfunc='first', fill_value=0)
-    pivot_table = pivot_table.reindex(sorted(pivot_table.columns), axis=1)
+            try:
+                rgb_value = webcolors.name_to_rgb(color_str)
+                light_rgb = tuple(int(min(255, c + 0.6 * (255 - c))) for c in rgb_value)
+            except ValueError:
+                # Handle invalid color names
+                light_rgb = (255, 255, 255)  # Use white color as default
+        light_hex = f"#{''.join(format(c, '02x') for c in light_rgb)}"  # Convert RGB to HEX color string
+        light_colors.append(light_hex)
 
-    # Replace non-zero values with column headers
-    pivot_table = pivot_table.apply(lambda x: np.where(x != 0, x.name, 'None'))
-    pivot_table['Actions_Done'] = pivot_table.apply(lambda row: row[row != 'None'].count(), axis=1)
-    pivot_table = pivot_table.merge(df[['identifiant_visite', 'visit_page_num']], on='identifiant_visite')
+    # Convert light_colors to NumPy array for comparison
+    light_colors = np.array(light_colors)
 
-    fig = px.sunburst(pivot_table, path=['Action_autre','Action_creation_compte','Page_Info','Page_Mordu','Page_OHdio','Page_accueil_Info','Page_accueil_Mordu','Page_accueil_OHdio','Video_Info','Video_Mordu'], values='visit_page_num', color='visit_page_num', branchvalues='total')
+    # Update marker colors with light colors
+    figure_data.marker.colors = [
+        light_colors[i % len(light_colors)] if color == light_colors[i % len(light_colors)] else color
+        for i, color in enumerate(figure_data.marker.colors)
+    ]
 
-    # # Show the plot
+    # Update the layout
+    fig.update_layout(
+        title='Account Created Journey Sunburst Chart',
+        coloraxis_colorbar=dict(
+            title='Account Creation Percentage',
+            len=0.5,
+            yanchor='bottom',
+            y=0.05,
+            tickformat='.2f%'
+        ),
+        legend=dict(
+            title="Account Created Journey",
+            itemsizing='constant'
+        )
+    )
+
     return fig
+    
