1b702932f1d01c09c622fcdb0214ad3cf5093bc1
DataVisualization Group6 <137502909+mrgdatastu@users.noreply.github.com>
2023-06-30 15:15:15 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ server = app.server
 app.title = 'Radio Canada Data Visualization Project | INF8808'
 
 dataframe = pd.read_csv('./RC50000.csv')
-#dataframe = pd.read_csv('f:/RC-poly_donnees_simulees.csv')
+#dataframe = pd.read_csv('./RC1000.csv')
 
 polar_fig = polar.generate_polar(dataframe)
 bar_fig = bar_chart.generate_bar_chart(dataframe)
