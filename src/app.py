8431889d62493e3e42a3fd2ade64eec892afa9fb
DataVisualization Group6 <137502909+mrgdatastu@users.noreply.github.com>
2023-06-30 02:42:56 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -13,13 +13,13 @@ app = Dash(__name__)
 server = app.server
 app.title = 'Radio Canada Data Visualization Project | INF8808'
 
-#dataframe = pd.read_csv('./RC1000-1.csv')
+dataframe = pd.read_csv('https://drive.google.com/file/d/14vdInfQ2GH-q_Lry0IqxeErBH6_BP35r/view?usp=drive_link')
 #Dataset Sample 1000 record
 #url = "https://drive.google.com/file/d/14vdInfQ2GH-q_Lry0IqxeErBH6_BP35r/view?usp=drive_link"
 #Dataset full 
-url = "https://drive.google.com/file/d/1nBE3ZpJxqr4esh6sgnTEF4PjAEbpIETn/view?usp=drive_link"
-s = requests.get(url).content
-dataframe = pd.read_csv(s)
+# url = "https://drive.google.com/file/d/1nBE3ZpJxqr4esh6sgnTEF4PjAEbpIETn/view?usp=drive_link"
+# s = requests.get(url).content
+# dataframe = pd.read_csv(s)
 
 polar_fig = polar.generate_polar(dataframe)
 bar_fig = bar_chart.generate_bar_chart(dataframe)
